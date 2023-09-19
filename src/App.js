bc9ea9d8e3abc042208c441717b3328597c5f59b
Zhenya <93037010+Zheny005@users.noreply.github.com>
2023-09-19 13:49:26 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Components/Header';
+import Body from './Components/Body';
+import Footer from './Components/Footer';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        Header / >
+        <
+        Body / >
+        <
+        Footer / >
+        <
+        /div>
+    );
+}
+
+export default App;
