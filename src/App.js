d73da696f84f96f96d15f613a1c22d89c7adf543
Zhenya <93037010+Zheny005@users.noreply.github.com>
2023-12-04 00:55:29 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import Header from './Components/Header';
-import Body from './Components/Body';
-import Footer from './Components/Footer';
+import { useState } from 'react';
+import { Header, Body, Footer, Menu } from './Components/import';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        Header / >
-        <
-        Body / >
-        <
-        Footer / >
-        <
-        /div>
+
+  const [items, setItems] = useState([
+    { id: 1, name: 'Mercedes-Benz w223', isChecked: false },
+    { id: 2, name: 'Mercedes-Benz w222', isChecked: false },
+    { id: 3, name: 'Mercedes-Benz w221', isChecked: false },
+    { id: 4, name: 'Mercedes-Benz w220', isChecked: false },
+  ]);
+
+  const handleCheckboxChange = (itemId) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId ? { ...item, isChecked: !item.isChecked } : item
+      )
     );
+  };
+
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      <Footer />
+
+      <ul className='ul-product'>
+        {items.map((item) => (
+          <li key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.isChecked}
+              onChange={() => handleCheckboxChange(item.id)}
+            />
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
